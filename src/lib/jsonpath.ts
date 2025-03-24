765802ad9093f84059ba8f9fbcb966f06ba05ab1
Kazuki Hamasaki <ne.vivam.si.abis@gmail.com>
2025-03-24 23:27:26 +0900

    Fixed #84 Output paths

--- src/lib/jsonpath.ts
+++ src/lib/jsonpath.ts
@@ -1,22 +1,38 @@
-import { Engine } from "@/types/engine";
+import type { Engine } from "@/types/engine";
 import { JSONPath } from "jsonpath-plus";
-import { JSONValue } from "@/types/json";
+import type { JSONValue } from "@/types/json";
 import { JSONPathJS } from "jsonpath-js";
 
 export const evalJsonPath = (
   engine: Engine,
   document: JSONValue,
   query: string
-) => {
+): { values: JSONValue[]; paths: string[] } => {
   switch (engine) {
-    case "rfc9535":
-      return new JSONPathJS(query).find(document);
-    case "jsonpath-plus":
-      return JSONPath({
+    case "rfc9535": {
+      const result = new JSONPathJS(query).paths(document);
+      return {
+        values: result.map((path) => path.value),
+        paths: result.map((path) => path.path),
+      };
+    }
+    case "jsonpath-plus": {
+      const results = JSONPath({
         json: document,
         path: query,
-      });
+        resultType: "all",
+      }) as {
+        path: string;
+        value: JSONValue;
+      }[];
+
+      return {
+        values: results.map((result) => result.value),
+        paths: results.map((result) => result.path),
+      };
+    }
     default:
       engine satisfies never;
+      throw new Error(`Unknown engine: ${engine}`);
   }
 };
